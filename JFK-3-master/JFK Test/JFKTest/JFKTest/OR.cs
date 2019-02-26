a2ab501fa5360f9b81b606ba6854ab04058592ed
PatronPL <45845986+PatronPL@users.noreply.github.com>
2019-02-26 21:36:56 +0100

    Add files via upload

---
+++ JFK-3-master/JFK Test/JFKTest/JFKTest/OR.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace JFKTest
+{
+    using Jfk.Prism;
+    [Description("Logiczny OR, alternatywa.")]
+    public class OR : ICallable
+    {
+        public int Call(int arg1, int arg2)
+        {
+            return (arg1 | arg2);
+        }
+        //[Description("Przesunięcie logiczne w lewo o wartość z drugiego argumentu.")]
+        public int MoveLeft(int arg1, int arg2)
+        {
+            return (arg1 << arg2);
+        }
+        //[Description("Przesunięcie logiczne w prawo o wartość z drugiego argumentu.")]
+        public int MoveRight(int arg1, int arg2)
+        {
+            return (arg1 >> arg2);
+        }
+    }
+}
---
+++ JFK-3-master/JFKTest2/MoveRight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JFKTest2
+{
+    using Jfk.Prism;
+    [Description("Przesunięcie logiczne w prawo o wartość z drugiego argumentu.")]
+    public class MoveRight : ICallable
+    {
+        public int Call(int arg1, int arg2)
+        {
+            return (arg1 >> arg2);
+        }
+    }
+}
